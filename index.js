5c6c0de249eabba8faebafffe5ba0786e5d8a6cf
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-10-03 20:36:22 +0600

    sum and foreich and map

--- index.js
+++ index.js
@@ -12,3 +12,24 @@ console.log("amader desh " + "dada der desh");
 console.log(typeof ("akfk"));
 
 console.log(Math.round(Math.random() * 100));
+
+
+const Arr = [223, 3, 3, 4, 4, 4, 11, 4, 4, 4]
+
+Arr.forEach((item) => {
+    if (item % 2 === 0) {
+        console.log(item);
+    }
+
+})
+
+const evenArr = Arr.map((item) => {
+    const oddArr = []
+    if (item % 2 === 0) {
+        oddArr.push(item)
+    }
+
+    return oddArr
+})
+
+console.log(evenArr, "evenArr");
